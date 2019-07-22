a37ce380b4a3d34c66d657a3146f0399ddf3d9e2
slabnevitch <handofschpilrain@mail.ru>
2019-07-22 23:02:35 +0500

    svetlana added block in progress

--- js/common.js
+++ js/common.js
@@ -112,6 +112,32 @@ jQuery(function() {
 					}
 				}
 			});
+
+					$('.model-product__display-slide').magnificPopup({
+						type: 'image',
+						preloader: true,
+						focus: '#name',
+						closeOnContentClick: false,
+						closeBtnInside: false,
+						mainClass: 'mfp-with-zoom mfp-img-mobile',
+						image: {
+							verticalFit: true,
+							titleSrc: function(item) {
+								return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+							}
+						},
+						gallery: {
+							enabled: true
+						},
+						zoom: {
+							enabled: true,
+					duration: 300, // don't foget to change the duration also in CSS
+					opener: function(element) {
+						return element.find('img');
+					}
+				}
+				
+			});
 		// end magnific-popup
 
 
